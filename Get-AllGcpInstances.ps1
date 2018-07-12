501cbd9e03b4cc8bf4af1ec4d2b9354b23b40723
Robert Margeson <13839818+margeson@users.noreply.github.com>
2018-07-12 15:05:20 -0400

    Get-AllGcpInstances.ps1
    PowerShell script to obtain a list of every running instance in GCP across all projects

---
+++ Get-AllGcpInstances.ps1
@@ -0,0 +1,13 @@
+﻿# Get Project ID (doesn't filter activte accounts)
+foreach ($project in gcloud projects list --format="value(projectId)") {
+    
+    # Create headers in CSV file
+    echo Project Name,Instance >> All-Powered-On-Gcp-Instances.csv
+    
+    # Get all powered on instances in Project
+    $instances = Get-GceInstance -Project $Project | Where-Object status -eq running | foreach { $_.name }
+        
+        # Append CSV file with each instance per project
+        foreach ($instance in $instances) {
+            "$project,$instance" >> All-Powered-On-Gcp-Instances.csv }
+  }
